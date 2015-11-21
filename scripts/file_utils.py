a57a519163a19f85829a157c2c278dec666f1394
Blake Wulfe <blake.w.wulfe@gmail.com>
2015-11-21 12:40:49 -0800

    adding model saving functions

--- scripts/file_utils.py
+++ scripts/file_utils.py
@@ -1,14 +1,17 @@
 """
-:description: save and load weight vectors
+:description: save and load weight vectors and theano models
 """
 
 import os
 import pickle
+import cPickle
 import datetime
 
-def save_weights(weights):
+def save_weights(weights, episodic=False):
 
 	output_filename = "weights/{}.pkl".format(datetime.datetime.now().isoformat())
+	if episodic:
+		output_filename = "weights/episodic-{}.pkl".format(datetime.datetime.now().isoformat())
 	with open(output_filename, 'wb') as f:
 		pickle.dump(weights, f)
 
@@ -27,3 +30,14 @@ def load_weights():
 	except IOError as e:
 		print("weight file {} not found, reinitializing weights".format(input_filename))
 	return weights
+
+def save_model(model, output_filename):
+	f = file(output_filename, 'wb')
+	cPickle.dump(model, f, protocol=cPickle.HIGHEST_PROTOCOL)
+	f.close()
+
+def load_model(model_filename):
+	f = file(model_filename, 'rb')
+	model = cPickle.load(f)
+	f.close()
+	return model
