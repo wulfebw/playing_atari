551001928279c35ee1b6acd5883821dc5aa94e9f
David Hershey <dshersh@stanford.edu>
2015-12-10 15:27:19 -0800

    Get minimal action set automatically

--- scripts/train_agent.py
+++ scripts/train_agent.py
@@ -31,18 +31,18 @@ RECORD_WEIGHTS = True
 RECORD_WEIGHTS_PERIOD = 25
 ######################################
 
-def train_agent(gamepath, agent, n_episodes, display_screen, record_weights, 
+def train_agent(gamepath, agent, n_episodes, display_screen, record_weights,
         reduce_exploration_prob_amount, n_frames_to_skip):
     """
-    :description: trains an agent to play a game 
+    :description: trains an agent to play a game
 
-    :type gamepath: string 
+    :type gamepath: string
     :param gamepath: path to the binary of the game to be played
 
     :type agent: subclass RLAlgorithm
     :param agent: the algorithm/agent that learns to play the game
 
-    :type n_episodes: int 
+    :type n_episodes: int
     :param n_episodes: number of episodes of the game on which to train
     """
 
@@ -72,7 +72,7 @@ def train_agent(gamepath, agent, n_episodes, display_screen, record_weights,
     best_reward = 0
     print('starting training...')
     for episode in xrange(n_episodes):
-        action = 0  
+        action = 0
         reward = 0
         newAction = None
 
@@ -81,12 +81,12 @@ def train_agent(gamepath, agent, n_episodes, display_screen, record_weights,
         lives = ale.lives()
 
         screen = np.zeros((32, 32, 3), dtype=np.int8)
-        state = { "screen" : screen, 
-                "objects" : None, 
-                "prev_objects": None, 
-                "prev_action": 0, 
+        state = { "screen" : screen,
+                "objects" : None,
+                "prev_objects": None,
+                "prev_action": 0,
                 "action": 0 }
-        
+
         while not ale.game_over():
             # if newAction is None then we are training an off-policy algorithm
             # otherwise, we are training an on policy algorithm
@@ -103,10 +103,10 @@ def train_agent(gamepath, agent, n_episodes, display_screen, record_weights,
 
             new_screen = ale.getScreenRGB()
             new_screen = screen_preprocessor.preprocess(new_screen)
-            new_state = {"screen": new_screen, 
-                        "objects": None, 
-                        "prev_objects": state["objects"], 
-                        "prev_action": state["action"], 
+            new_state = {"screen": new_screen,
+                        "objects": None,
+                        "prev_objects": state["objects"],
+                        "prev_action": state["action"],
                         "action": action}
             newAction = agent.incorporateFeedback(state, action, reward, new_state)
 
@@ -133,27 +133,32 @@ def train_agent(gamepath, agent, n_episodes, display_screen, record_weights,
             print('average feature weight: {}'.format(avg_feat_weight))
             print '############################'
             print '############################\n'
-            
+
         if episode != 0 and episode % RECORD_WEIGHTS_PERIOD == 0 and record_weights:
             file_utils.save_rewards(rewards, filename='episode-{}-{}-rewards'.format(episode, type(agent).__name__))
             file_utils.save_weights(agent.weights, filename='episode-{}-{}-weights'.format(episode, type(agent).__name__))
 
         if agent.explorationProb > MINIMUM_EXPLORATION_EPSILON:
             agent.explorationProb -= reduce_exploration_prob_amount
-        
+
         print('episode: {} ended with score: {}'.format(episode, total_reward))
         ale.reset_game()
     return rewards
 
 if __name__ == '__main__':
-    game = 'breakout.bin'
+    game = 'asterix.bin'
     gamepath = os.path.join('roms', game)
     agent = LEARNING_ALGORITHM
+    ale = ALEInterface()
+    ale.loadROM(gamepath)
+    actions = ale.getMinimalActionSet()
+    agent.actions = actions;
+    print actions
     if LOAD_WEIGHTS:
         agent.weights = file_utils.load_weights(WEIGHTS_FILENAME)
-    rewards = train_agent(gamepath, agent, 
-                        n_episodes=NUM_EPISODES, 
-                        display_screen=DISPLAY_SCREEN, 
-                        record_weights=RECORD_WEIGHTS, 
+    rewards = train_agent(gamepath, agent,
+                        n_episodes=NUM_EPISODES,
+                        display_screen=DISPLAY_SCREEN,
+                        record_weights=RECORD_WEIGHTS,
                         reduce_exploration_prob_amount=EXPLORATION_REDUCTION_AMOUNT,
                         n_frames_to_skip=NUM_FRAMES_TO_SKIP)
