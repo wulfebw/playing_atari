66b65e86e1f010cf0659e18594ae95a19e9f2950
Ubuntu <ubuntu@ip-10-197-185-217.us-west-2.compute.internal>
2015-12-23 07:26:03 +0000

    changes to get intrinsic running on aws

--- scripts/intrinsic/run_nature.py
+++ scripts/intrinsic/run_nature.py
@@ -22,8 +22,8 @@ class Defaults:
     # ----------------------
     # ALE Parameters
     # ----------------------
-    BASE_ROM_PATH = "../roms/"
-    ROM = 'breakout.bin'
+    BASE_ROM_PATH = "../../roms/"
+    ROM = 'montezuma_revenge.bin'
     FRAME_SKIP = 4
     REPEAT_ACTION_PROBABILITY = 0
 
@@ -50,7 +50,7 @@ class Defaults:
     UPDATE_FREQUENCY = 4
     REPLAY_MEMORY_SIZE = 1000000
     BATCH_SIZE = 32
-    NETWORK_TYPE = "nature_dnn"
+    NETWORK_TYPE = "nature_cuda"
     FREEZE_INTERVAL = 10000
     REPLAY_START_SIZE = 50000
     RESIZE_METHOD = 'scale'
